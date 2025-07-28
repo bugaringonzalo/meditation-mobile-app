2b24c27fe0e317804cba47616d24f902ca489f67
Gonzalo Bugarin <bugaringonzalo@gmail.com>
2025-07-28 00:26:24 -0300

    up to meditating standard 10 secs

---
+++ app/(tabs)/meditate/[id].tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useMemo, useState } from "react";
+import { ImageBackground, Pressable, Text, View } from "react-native";
+
+// import de las metitaiton images
+import AppGradient from "@/components/AppGradient";
+import MEDITATION_IMAGES from "@/constants/meditation-images";
+import { router, useLocalSearchParams } from "expo-router";
+
+// import vector icon
+import { CustomButton } from "@/components/CustomButton";
+import { AntDesign } from "@expo/vector-icons";
+
+const Meditate = () => {
+  const { id } = useLocalSearchParams();
+  const [secondsRemaining, setSecondsRemaining] = useState(10);
+  const [isMeditating, setIsMeditating] = useState(false);
+
+  useMemo(() => {
+    // Reset the timer when the component mounts or id changes
+    setSecondsRemaining(10);
+    setIsMeditating(false);
+  }, [id]);
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout;
+
+    // Exit
+    if (secondsRemaining === 0) {
+      setIsMeditating(false);
+      return;
+    }
+
+    if (isMeditating) {
+      // Start the meditation countdown
+      timerId = setTimeout(() => {
+        setSecondsRemaining(secondsRemaining - 1);
+      }, 1000);
+    }
+
+    return () => clearTimeout(timerId);
+  }, [secondsRemaining, isMeditating]);
+
+  // Format the time left to ensure two digits are displayed
+  const formattedTimeMinutes = String(
+    Math.floor(secondsRemaining / 60)
+  ).padStart(2, "0");
+  const formattedTimeSeconds = String(secondsRemaining % 60).padStart(2, "0");
+
+  return (
+    <View className="flex-1">
+      <ImageBackground
+        source={MEDITATION_IMAGES[Number(id) - 1]}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <AppGradient colors={["transparent", "rgba(0, 0, 0, 0.8)"]}>
+          <Pressable
+            onPress={() => {
+              router.back();
+            }}
+            className="absolute top-16 left-6 z-10"
+          >
+            <AntDesign name="leftcircleo" size={50} color="white" />
+          </Pressable>
+          <View className="flex-1 justify-center">
+            <View className="mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center">
+              <Text className="text-4xl text-blue-800 font-rmono">
+                {formattedTimeMinutes}:{formattedTimeSeconds}
+              </Text>
+            </View>
+          </View>
+
+          <View className="mb-5">
+            <CustomButton
+              title="Start Meditation"
+              onPress={() => {
+                setIsMeditating(!isMeditating);
+              }}
+            />
+          </View>
+        </AppGradient>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default Meditate;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,35 @@
-import { Slot, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { Slot, SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import "../global.css";
 
+// Prevent the splash screen from auto-hiding until the fonts are loaded
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "Roboto-Mono": require("../assets/fonts/RobotoMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) {
+      console.error("Error loading fonts:", error);
+    }
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) {
+    return null; // Render nothing while fonts are loading
+  }
+  if (!fontsLoaded && !error) {
+    return null; // Render nothing while fonts are loading
+  }
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      {/* <Stack.Screen name="meditate" options={{ headerShown: false }} /> */}
+      <Stack.Screen name="meditate/[id]" options={{ headerShown: false }} />
       <Slot />
     </Stack>
   );
